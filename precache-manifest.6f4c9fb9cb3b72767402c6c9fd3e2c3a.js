3836a5bbcfc719f16be70d07bfca1a6dd90a2a3e
Dan S <63663822+Phoenka@users.noreply.github.com>
2020-06-30 19:44:24 +0200

    Updates

---
+++ precache-manifest.6f4c9fb9cb3b72767402c6c9fd3e2c3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1339255e87dcf17b81bee753d3aa8bf",
+    "url": "/Second-React-App/index.html"
+  },
+  {
+    "revision": "e24c5cd6bcf8e2d411ed",
+    "url": "/Second-React-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/Second-React-App/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Second-React-App/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e24c5cd6bcf8e2d411ed",
+    "url": "/Second-React-App/static/js/main.a298cc6c.chunk.js"
+  },
+  {
+    "revision": "5f9fd0c81909f91e84c8",
+    "url": "/Second-React-App/static/js/runtime-main.28ecea89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Second-React-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
